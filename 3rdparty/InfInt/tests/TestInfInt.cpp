7221bbb44349e663244b9a99916102a94d19520b
IohannesFolbort <petrarce@gmail.com>
2024-06-25 09:55:29 +0200

    Add tests for InfInt

---
+++ 3rdparty/InfInt/tests/TestInfInt.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_MODULE TestInfInt
+#include <InfInt.h>
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(InfIntTest) {
+  InfInt v1 = "333333333333333333333333333333";
+  InfInt v2 = "-333333333333333333333333333333";
+  InfInt res = v1 + v2;
+  BOOST_CHECK(res.toString() == "0");
+  res = v1 - v2;
+  BOOST_CHECK(res.toString() == "666666666666666666666666666666");
+  BOOST_CHECK((v1 * 2).toString() == "666666666666666666666666666666");
+  BOOST_CHECK(res / 2 == v1);
+}
--- default.cmake
+++ default.cmake
@@ -1,5 +1,4 @@
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake")
-message("CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}")
 
 include(CTest)
 include(AddBoostTests)
