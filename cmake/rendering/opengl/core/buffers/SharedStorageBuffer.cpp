1469cafce40e91fc0c7cfe32b67ad006a0e45cc9
IohannesFolbort <petrarce@gmail.com>
2025-05-27 20:52:13 +0200

    initial implementation of SSBO wrapper

---
+++ cmake/rendering/opengl/core/buffers/SharedStorageBuffer.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ivan on 5/27/25.
+//
+
+#include "SharedStorageBuffer.h"
+
+namespace core_gfx
+{
+namespace open_gl
+{
+} // namespace open_gl
+} // namespace core_gfx
---
+++ cmake/rendering/opengl/core/buffers/SharedStorageBuffer.h
@@ -0,0 +1,20 @@
+//
+// Created by ivan on 5/27/25.
+//
+
+#ifndef SHAREDSTORAGEBUFFER_H
+#define SHAREDSTORAGEBUFFER_H
+
+namespace core_gfx
+{
+namespace open_gl
+{
+
+class SharedStorageBuffer
+{
+};
+
+} // namespace open_gl
+} // namespace core_gfx
+
+#endif // SHAREDSTORAGEBUFFER_H
