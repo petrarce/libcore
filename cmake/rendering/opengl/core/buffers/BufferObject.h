0b439675e0e7c826f8abedae1ba3a6abf0a69852
IohannesFolbort <petrarce@gmail.com>
2025-05-28 07:09:25 +0200

    feat: add MapBuffer template for OpenGL buffer mapping

--- cmake/rendering/opengl/core/buffers/BufferObject.h
+++ cmake/rendering/opengl/core/buffers/BufferObject.h
@@ -8,23 +8,51 @@
 namespace core_gfx {
 namespace open_gl {
 
-template<GLenum Target>
-class BufferObject : public BufferObjectBase {
+template<class T>
+concept InBufferObject = std::is_base_of_v<BufferObjectBase, T>;
+
+// add documentation here, ai!
+template<InBufferObject T>
+class MapBuffer
+{
 public:
-    template<typename T>
-    explicit BufferObject(const std::vector<T>& data);
+
+    template<GLuint AccessType = GL_READ_ONLY>
+    explicit MapBuffer(T& buffer)
+        : mBuffer(buffer)
+    {
+        glBindBuffer(T::TargetValue, mBuffer.m_id);
+        mBufferPtr = glMapBuffer(T::TargetValue, AccessType);
+    }
+    ~MapBuffer()
+    {
+        if (mBufferPtr == nullptr)
+            return;
+        glBindBuffer(T::TargetValue, mBuffer.m_id);
+        glUnmapBuffer(T::TargetValue);
+    }
+    void* RawPointer() const { return mBufferPtr; }
+private:
+    T& mBuffer;
+    void* mBufferPtr = nullptr;
 };
 
-template<GLenum Target>
-template<typename T>
-BufferObject<Target>::BufferObject(const std::vector<T>& data) {
-    glBindBuffer(Target, m_id);
-    glBufferData(Target, 
-                data.size() * sizeof(T),
-                data.data(),
-                GL_STATIC_DRAW);
-}
-
+template<GLenum Tgt>
+class BufferObject : public BufferObjectBase {
+    friend MapBuffer<BufferObject<Tgt>>;
+public:
+    using TargetValue = Tgt;
+public:
+    template<GLuint T, GLuint AccessHint = GL_STATIC_DRAW>
+    explicit BufferObject(const std::vector<T>& data)
+    {
+        glBindBuffer(Tgt, m_id);
+        glBufferData(Tgt,
+                     data.size() * sizeof(T),
+                     data.data(),
+                     AccessHint);
+    }
+};
 } // namespace open_gl
 } // namespace core_gfx
 
