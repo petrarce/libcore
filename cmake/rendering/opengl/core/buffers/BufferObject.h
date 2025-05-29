9b31ad684bfce72575f48dee71572896eb1eea81
IohannesFolbort <petrarce@gmail.com>
2025-05-29 09:03:09 +0200

    refactor classes

--- cmake/rendering/opengl/core/buffers/BufferObject.h
+++ cmake/rendering/opengl/core/buffers/BufferObject.h
@@ -45,52 +45,16 @@ public:
 	BufferObject& operator=(const BufferObject&) = delete;
 
 	// Allow moving
-	BufferObject(BufferObject&& other) noexcept
-		: BufferObjectBase(std::move(other))
-		, mAccessHint(other.mAccessHint)
-	{
-	}
-
-	BufferObject& operator=(BufferObject&& other) noexcept
-	{
-		if (this != &other)
-		{
-			BufferObjectBase::operator=(std::move(other));
-			mAccessHint = other.mAccessHint;
-		}
-		return *this;
-	}
+	BufferObject(BufferObject&& other) noexcept;
+	BufferObject& operator=(BufferObject&& other) noexcept;
 
 	template<typename T>
-	explicit BufferObject(const std::vector<T>& data, GLenum accessHint = GL_STATIC_DRAW)
-		: mAccessHint(accessHint)
-	{
-		if (data.empty())
-		{
-			throw std::invalid_argument("Cannot create buffer with empty data");
-		}
-		glBindBuffer(Tgt, m_id);
-		glBufferData(Tgt, data.size() * sizeof(T), data.data(), mAccessHint);
-	}
+	explicit BufferObject(const std::vector<T>& data, GLenum accessHint = GL_STATIC_DRAW);
 
 	template<typename T>
-	void UpdateData(const std::vector<T>& data, GLintptr offset = 0)
-	{
-		if (data.empty())
-		{
-			throw std::invalid_argument("Cannot update buffer with empty data");
-		}
-		glBindBuffer(Tgt, m_id);
-		glBufferSubData(Tgt, offset, data.size() * sizeof(T), data.data());
-	}
+	void UpdateData(const std::vector<T>& data, GLintptr offset = 0);
 
-	size_t GetSize() const noexcept
-	{
-		GLint size = 0;
-		glBindBuffer(Tgt, m_id);
-		glGetBufferParameteriv(Tgt, GL_BUFFER_SIZE, &size);
-		return static_cast<size_t>(size);
-	}
+	size_t GetSize() const noexcept;
 
 private:
 	GLenum mAccessHint{ GL_STATIC_DRAW };
@@ -120,63 +84,122 @@ class MapBuffer
 	static_assert(IsBufferObject<T>, "Template parameter must be a BufferObject type");
 
 public:
-	explicit MapBuffer(T& buffer, GLenum accessHint = GL_READ_ONLY)
-		: mBuffer(buffer)
-	{
-		glBindBuffer(T::TargetValue, mBuffer.m_id);
-		mBufferPtr = glMapBuffer(T::TargetValue, accessHint);
-		if (!mBufferPtr)
-		{
-			throw std::runtime_error("Failed to map buffer");
-		}
-	}
+	explicit MapBuffer(T& buffer, GLenum accessHint = GL_READ_ONLY) noexcept;
 
-	~MapBuffer() noexcept
-	{
-		if (mBufferPtr)
-		{
-			glBindBuffer(T::TargetValue, mBuffer.m_id);
-			glUnmapBuffer(T::TargetValue);
-		}
-	}
+	~MapBuffer() noexcept;
 
 	// Disallow copying
 	MapBuffer(const MapBuffer&) = delete;
 	MapBuffer& operator=(const MapBuffer&) = delete;
 
 	// Allow moving
-	MapBuffer(MapBuffer&& other) noexcept
-		: mBuffer(other.mBuffer)
-		, mBufferPtr(other.mBufferPtr)
-	{
-		other.mBufferPtr = nullptr;
-	}
+	MapBuffer(MapBuffer&& other) noexcept;
+	MapBuffer& operator=(MapBuffer&& other) noexcept;
 
-	MapBuffer& operator=(MapBuffer&& other) noexcept
-	{
-		if (this != &other)
-		{
-			mBuffer = other.mBuffer;
-			mBufferPtr = other.mBufferPtr;
-			other.mBufferPtr = nullptr;
-		}
-		return *this;
-	}
-
-	[[nodiscard]] void* RawPointer() const noexcept { return mBufferPtr; }
-
+	[[nodiscard]] void* RawPointer() const noexcept;
 	template<typename U>
-	[[nodiscard]] U* As() const noexcept
-	{
-		return static_cast<U*>(mBufferPtr);
-	}
+	[[nodiscard]] U* As() const noexcept;
 
 private:
 	T& mBuffer;
 	void* mBufferPtr{ nullptr };
 };
+//================== BufferObject definitions ======================================================
 
+template<GLenum Tgt>
+BufferObject<Tgt>::BufferObject(BufferObject&& other) noexcept
+	: BufferObjectBase(std::move(other))
+	, mAccessHint(other.mAccessHint)
+{
+}
+template<GLenum Tgt>
+BufferObject<Tgt>& BufferObject<Tgt>::operator=(BufferObject&& other) noexcept
+{
+	if (this != &other)
+	{
+		BufferObjectBase::operator=(std::move(other));
+		mAccessHint = other.mAccessHint;
+	}
+	return *this;
+}
+template<GLenum Tgt>
+template<typename T>
+BufferObject<Tgt>::BufferObject(const std::vector<T>& data, GLenum accessHint)
+	: mAccessHint(accessHint)
+{
+	if (data.empty())
+		return;
+	glBindBuffer(Tgt, m_id);
+	glBufferData(Tgt, data.size() * sizeof(T), data.data(), mAccessHint);
+}
+template<GLenum Tgt>
+template<typename T>
+void BufferObject<Tgt>::UpdateData(const std::vector<T>& data, GLintptr offset)
+{
+	if (data.empty())
+		return;
+	glBindBuffer(Tgt, m_id);
+	glBufferSubData(Tgt, offset, data.size() * sizeof(T), data.data());
+}
+template<GLenum Tgt>
+size_t BufferObject<Tgt>::GetSize() const noexcept
+{
+	GLint size = 0;
+	glBindBuffer(Tgt, m_id);
+	glGetBufferParameteriv(Tgt, GL_BUFFER_SIZE, &size);
+	return static_cast<size_t>(size);
+}
+
+// ===============================MapBuffer definitions ============================================
+template<class T>
+MapBuffer<T>::MapBuffer(T& buffer, GLenum accessHint) noexcept : mBuffer(buffer)
+{
+	glBindBuffer(T::TargetValue, mBuffer.m_id);
+	mBufferPtr = glMapBuffer(T::TargetValue, accessHint);
+}
+template<class T>
+MapBuffer<T>::~MapBuffer() noexcept
+{
+	if (!mBufferPtr)
+		return;
+	glBindBuffer(T::TargetValue, mBuffer.m_id);
+	glUnmapBuffer(T::TargetValue);
+}
+template<class T>
+MapBuffer<T>::MapBuffer(MapBuffer&& other) noexcept
+	: mBuffer(other.mBuffer)
+	, mBufferPtr(other.mBufferPtr)
+{
+	other.mBufferPtr = nullptr;
+}
+template<class T>
+MapBuffer<T>& MapBuffer<T>::operator=(MapBuffer&& other) noexcept
+{
+	if (this == &other)
+		return *this;
+
+	mBuffer = other.mBuffer;
+	mBufferPtr = other.mBufferPtr;
+	other.mBufferPtr = nullptr;
+
+	return *this;
+}
+template<class T>
+void* MapBuffer<T>::RawPointer() const noexcept
+{
+	return mBufferPtr;
+}
+template<class T>
+template<typename U>
+U* MapBuffer<T>::As() const noexcept
+{
+	return static_cast<U*>(mBufferPtr);
+}
+
+// =====================================Convenience aliases definitions ============================
 using ShaderStorageBuffer = BufferObject<GL_SHADER_STORAGE_BUFFER>;
+
+// ========================================= Concept tests =========================================
 static_assert(IsBufferObject<ShaderStorageBuffer>);
 static_assert(!IsBufferObject<int>);
 
