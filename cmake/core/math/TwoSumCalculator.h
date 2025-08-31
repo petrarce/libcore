420f306724b670b0d2e68dcc0d806bbcd07941c5
IohannesFolbort <petrarce@gmail.com>
2025-08-31 08:37:25 +0200

    One path map traversal

--- cmake/core/math/TwoSumCalculator.h
+++ cmake/core/math/TwoSumCalculator.h
@@ -4,6 +4,8 @@
 
 #ifndef TWOSUMCALCULATOR_H
 #define TWOSUMCALCULATOR_H
+#include "core/utils/StrongTypedef.h"
+
 #include <algorithm>
 #include <numeric>
 #include <optional>
@@ -11,78 +13,61 @@
 #include <absl/strings/str_format.h>
 #include <boost/mpl/assert.hpp>
 #include <iostream>
-namespace libcore {
-namespace solutions {
+#include <absl/container/flat_hash_map.h>
+namespace libcore
+{
+namespace solutions
+{
 namespace detail
 {
-class Solution {
+class Solution
+{
 
 public:
-
-std::vector<int> twoSum(const std::vector<int>& nums, int target)
-{
-	if (nums.empty())
-		return {};
-
-	if (nums.size() == 1)
-		return nums[0] == target
-			? std::vector{0}
-			: std::vector<int>{};
-
-
-	if (nums.size() == 2 )
-		return nums[0] + nums[1] != target
-			? std::vector<int>{}
-			: std::vector{0, 1};
-	// O(n log_n) traversal
-	std::vector<int> indices(nums.size());
-	std::iota(indices.begin(), indices.end(), 0);
-	std::sort(
-		indices.begin(),
-		indices.end(),
-		[&](const int l, const int r) { return nums[l] < nums[r]; });
-	for (int i = 0; i < indices.size(); ++i)
+	std::vector<int> twoSum(const std::vector<int>& nums, int target)
 	{
-		const auto v1idx = indices[i];
-		const auto v1 = nums[v1idx];
-		const auto potential_v2idx_iter = std::lower_bound(
-			indices.begin() + i + 1,
-			indices.end(),
-			target,
-			[&](auto v2idx, auto) { return v1 + nums[v2idx] < target; });
+		if (nums.empty())
+			return {};
 
-		if (potential_v2idx_iter == indices.end())
-			continue;
+		if (nums.size() == 1)
+			return nums[0] == target ? std::vector{ 0 } : std::vector<int>{};
 
-		if (nums[*potential_v2idx_iter] + v1 != target)
-			continue;
-
-		return v1idx > *potential_v2idx_iter
-			? std::vector{ *potential_v2idx_iter, v1idx }
-			: std::vector{ v1idx, *potential_v2idx_iter };
+		if (nums.size() == 2)
+			return nums[0] + nums[1] != target ? std::vector<int>{} : std::vector{ 0, 1 };
+		// O(n) one-path map traversal
+		absl::flat_hash_map<int, int> valueMap;
+		for (int idx1 = 0; idx1 < nums.size(); idx1++)
+		{
+			const auto rest = target - nums[idx1];
+			if (valueMap.contains(rest))
+			{
+				const auto idx2 = valueMap[rest];
+				return idx1 > idx2 ? std::vector{ idx2, idx1 } : std::vector{ idx1, idx2 };
+			}
+			valueMap.insert(std::pair{ nums[idx1], idx1 });
+		}
+		return {};
 	}
-	return {};
-}
-
 };
 
-}
+} // namespace detail
 
-class two_sum_calculator {
+class two_sum_calculator
+{
 public:
-	static std::optional<std::pair<int, int>> solve(const std::vector<int>& nums, int target)
+	static std::optional<std::pair<int, int> > solve(const std::vector<int>& nums, int target)
 	{
 		const auto solution = detail::Solution().twoSum(nums, target);
 		assert(solution.size() <= 2);
 		if (solution.empty())
 			return std::nullopt;
 		if (solution.size() == 1)
-			return std::pair{solution[0], solution[0]};
-		return  std::optional(std::pair{solution[0], solution[1]});
+			return std::pair{ solution[0], solution[0] };
+		return std::optional(std::pair{ solution[0], solution[1] });
 	}
 };
 
-} // solutions
-} // libcore
+} // namespace solutions
+} // namespace libcore
 
-#endif //TWOSUMCALCULATOR_H
+#endif // TWOSUMCALCULATOR_H
