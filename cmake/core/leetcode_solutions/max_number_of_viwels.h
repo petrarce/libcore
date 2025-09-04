107e9ccc424a8e05d4e4ed189fbd35054729ee80
IohannesFolbort <petrarce@gmail.com>
2025-09-04 18:32:22 +0200

    max vowels

---
+++ cmake/core/leetcode_solutions/max_number_of_viwels.h
@@ -0,0 +1,22 @@
+//
+// Created by ifolbort on 9/4/25.
+//
+
+#ifndef MAX_NUMBER_OF_VIWELS_H
+#define MAX_NUMBER_OF_VIWELS_H
+#include <string>
+namespace libcore
+{
+namespace solutions
+{
+
+class max_number_of_viwels
+{
+public:
+	static int solve(const std::string& str, int k);
+};
+
+} // namespace solutions
+} // namespace libcore
+
+#endif // MAX_NUMBER_OF_VIWELS_H
